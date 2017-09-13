2f41cc4367914de24e01bc2e662def97d4077f72
xStaticVoid <dthurston1996@gmail.com>
2017-09-13 19:23:28 -0400

    Adding starter client code

---
+++ filetransferclient.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+
+class ftclient{
+    public static void main(String args[]){
+        try{
+            SocketChannel sc = SocketChannel.open();
+            Console cons = System.console();
+            String ip = cons.readLine("Enter IP address: ");
+            int port = Integer.parseInt(cons.readLine("Enter port number: "));
+            sc.connect(new InetSocketAddress(ip,port));
+            
+        }catch(IOexception eP){
+            System.out.println("Got an exception");
+        }
+    }
+}
