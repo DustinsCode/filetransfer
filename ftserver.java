71fb4e5ee3c47e551026f055f7ebc55a29c3fff0
Dustin Thurston <dthurston1996@gmail.com>
2017-09-24 11:49:12 -0400

    Server now supports multithreading and client can stay connected until exit command is entered.

--- ftserver.java
+++ ftserver.java
@@ -36,87 +36,109 @@ class ftserver{
                 if(sc != null){
                     System.out.println("A client has connected!");
                 }
-                ByteBuffer buffer = ByteBuffer.allocate(32768);
-                sc.read(buffer);
-                String fileName = new String(buffer.array());
-                fileName = fileName.trim();
-                if(fileName != null){
-                    try{
-                        System.out.println("Client trying to recieve " + fileName);
-                        
-                        //InputStream is = ftserver.class.getResourceAsStream(fileName);
-                        try{
-
-                            //testing
-                            Path filelocation = null;
-                            String l = null;
-                            try{
-                                filelocation = Paths.get(ftserver.class.getResource(fileName).toURI());
-                                File f = new File(filelocation.toString());
-                                
-                                String incoming = "incoming";
-                                buffer = ByteBuffer.wrap(incoming.getBytes());
-                                sc.write(buffer);
-                                
-                                //wait until client sends ready to recieve message
-                                buffer = ByteBuffer.allocate(4096);
-                                sc.read(buffer);
-                                buffer.compact();
-                                //String sendIt = new String(buffer.array());
-                                //sendIt = sendIt.trim();
-
-                                //sends length of file and then waits until client accepts it
-                                Long size = f.length();
-                                String fileSize = size.toString();
-                                System.out.println(fileSize);
-                                buffer = ByteBuffer.wrap(fileSize.getBytes());
-
-                                sc.write(buffer);
-                                buffer = ByteBuffer.allocate(4096);
-                                sc.read(buffer);
-
-                                byte[] fileBytes;
-                                FileInputStream fis = new FileInputStream(f);
-                                BufferedInputStream bis = new BufferedInputStream(fis);
-                                long bytesRead = 0;
-                                while(bytesRead != size){
-                                    int bytesToSend = 4096;
-                                    if(size - bytesRead >= bytesToSend){
-                                        bytesRead += bytesToSend;
-                                    }else{
-                                        bytesToSend = (int)(size-bytesRead);
-                                        bytesRead = size;
-                                    }
-                                    fileBytes = new byte[bytesToSend];
-                                    bis.read(fileBytes, 0, bytesToSend);
-                                    buffer = ByteBuffer.wrap(fileBytes);
-                                    sc.write(buffer);
-                                }
-                            }catch(URISyntaxException u){
-                                System.out.println("Error converting file");
-                            }
-                            
-                            System.out.println("The file has been sent.");
-                        }catch(IOException ioe){
-                            String error = "error";
-
-                            //tells client an error occurred
-                            buffer = ByteBuffer.wrap(error.getBytes());
-                            sc.write(buffer);
-                            
-                        }
-                    }catch(NullPointerException npe){
-                        String error = "filenotfound";
-                        System.out.println("The client's file doesn't exist.");
-                        
-                        buffer = ByteBuffer.wrap(error.getBytes());
-                        sc.write(buffer);
-                    }
-                }
+                serverThread s = new serverThread(sc);
+                s.start();
             }
         }catch(IOException e){
             System.out.println("Got an IO exception. Closing program...");
             return;
+        }
+    }
+}
+
+class serverThread extends Thread{
+    SocketChannel sc;
+    serverThread(SocketChannel channel){
+        sc = channel;
+    }
+    public void run(){
+        while(true){
+            try{
+                ByteBuffer buffer = ByteBuffer.allocate(32768);
+                    sc.read(buffer);
+                    String fileName = new String(buffer.array());
+                    fileName = fileName.trim();
+                    System.out.println(fileName);
+
+                    //exit, ls, and file request commands
+                    if(fileName.equals("exit")){
+                        System.out.println("Client disconnected");
+                        sc.close();
+                        return;
+                    }else if(fileName.equals("ls")){
+                        
+                    }else{
+                        try{
+                            System.out.println("Client trying to recieve " + fileName);
+                            
+                            try{
+
+                                Path filelocation = null;
+                                String l = null;
+                                try{
+                                    filelocation = Paths.get(ftserver.class.getResource(fileName).toURI());
+                                    File f = new File(filelocation.toString());
+                                    
+                                    String incoming = "incoming";
+                                    buffer = ByteBuffer.wrap(incoming.getBytes());
+                                    sc.write(buffer);
+                                    
+                                    //wait until client sends ready to recieve message
+                                    buffer = ByteBuffer.allocate(4096);
+                                    sc.read(buffer);
+                                    buffer.compact();
+
+                                    //sends length of file and then waits until client accepts it
+                                    Long size = f.length();
+                                    String fileSize = size.toString();
+                                    buffer = ByteBuffer.wrap(fileSize.getBytes());
+
+                                    sc.write(buffer);
+                                    buffer = ByteBuffer.allocate(4096);
+                                    sc.read(buffer);
+
+                                    byte[] fileBytes;
+                                    FileInputStream fis = new FileInputStream(f);
+                                    BufferedInputStream bis = new BufferedInputStream(fis);
+                                    long bytesRead = 0;
+                                    while(bytesRead != size){
+                                        int bytesToSend = 4096;
+                                        if(size - bytesRead >= bytesToSend){
+                                            bytesRead += bytesToSend;
+                                        }else{
+                                            bytesToSend = (int)(size-bytesRead);
+                                            bytesRead = size;
+                                        }
+                                        fileBytes = new byte[bytesToSend];
+                                        bis.read(fileBytes, 0, bytesToSend);
+                                        buffer = ByteBuffer.wrap(fileBytes);
+                                        sc.write(buffer);
+                                    }
+                                }catch(URISyntaxException u){
+                                    System.out.println("Error converting file");
+                                }
+                                
+                                System.out.println("The file has been sent.");
+                            }catch(IOException ioe){
+                                String error = "error";
+
+                                //tells client an error occurred
+                                buffer = ByteBuffer.wrap(error.getBytes());
+                                sc.write(buffer);
+                                
+                            }
+                        }catch(NullPointerException npe){
+                            String error = "filenotfound";
+                            System.out.println("The client's file doesn't exist.");
+                            
+                            buffer = ByteBuffer.wrap(error.getBytes());
+                            sc.write(buffer);
+                        }
+                    }
+                
+            }catch(IOException e){
+                System.out.println("Got an IO Exception");
+            }
         }
     }
 }
